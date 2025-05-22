c21c06739de2af6fc9a90e54c2cb394617339182
klevi0212 <klevi.smula@gmail.com>
2025-05-22 15:30:01 +0200

    lists ex done

---
+++ react/lists/src/Colors.jsx
@@ -0,0 +1,13 @@
+import Color from "./Color";
+
+export default function Colors({ colors }) {
+  return (
+    <>
+      <ul>
+        {colors.map((color) => (
+          <Color key={color.id} color={color} />
+        ))}
+      </ul>
+    </>
+  );
+}
